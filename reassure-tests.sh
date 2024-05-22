fbf41d82e0b8d68154db459c20d2a6233896a614
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-05-22 21:47:35 +0900

    edit tests.sh

--- reassure-tests.sh
+++ reassure-tests.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-set -e 
-BASELINE_BRANCH=${BASELINE_BRANCH:="head~1"}
-COMPARE_BRANCH=${COMPARE_BRANCH:="main"}
+set -e
+
+BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Required for `git switch` on CI
 git fetch origin
 
 # Gather baseline perf measurements
-git checkout "$BASELINE_BRANCH"
+git switch "$BASELINE_BRANCH"
 yarn install --force
 yarn reassure --baseline
 
-# Gather current perf measurements &  results
-git checkout "$COMPARE_BRANCH"
-
+# Gather current perf measurements & compare results
+git switch --detach -
 yarn install --force
-yarn reassure --branch
+yarn reassure
