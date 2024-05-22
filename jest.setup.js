34ed3c20c0da95e1586b653dd5e70b70fb43e561
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-05-22 19:50:00 +0900

    Exceeded timeout of 5000 ms for a test. 수정 jest.setup.js 작업

--- jest.setup.js
+++ jest.setup.js
@@ -5,3 +5,4 @@ configure({testingLibrary: 'react-native'});
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.setTimeout(30000);
