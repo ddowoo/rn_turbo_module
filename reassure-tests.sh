5c7472eae86f841d33f4c4cb27302f9860d87a7a
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-05-22 20:53:44 +0900

    chagne tests.sh

--- reassure-tests.sh
+++ reassure-tests.sh
@@ -7,13 +7,13 @@ BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 git fetch origin
 
 # Gather baseline perf measurements
-git switch "$BASELINE_BRANCH"
+git switch --detach -
 
 yarn install --force
 yarn reassure --baseline
 
 # Gather current perf measurements & compare results
-git switch --detach -
+git switch "$BASELINE_BRANCH"
 
 yarn install --force
 yarn reassure --branch
