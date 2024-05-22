3b21e8f4daf6e9b6b15873e2a8523018a9e1851f
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-05-22 20:10:10 +0900

    set baseline DefaultItemList

--- src/renderPerformanceOptimization/listPerformance.perf-test.tsx
+++ src/renderPerformanceOptimization/listPerformance.perf-test.tsx
@@ -3,7 +3,6 @@ import {test} from '@jest/globals';
 import {fireEvent, screen} from '@testing-library/react-native';
 import {measureRenders} from 'reassure';
 import DefaultItemList from './defaultList';
-import OptimizationList from './optimizationList';
 
 test('Flatlist 렌더링 최적화 전,후 비교', async () => {
   // 비교할 시나리오 작성
@@ -14,5 +13,5 @@ test('Flatlist 렌더링 최적화 전,후 비교', async () => {
   };
 
   // 기본 리스트 , 0 버전 대에서는 measurePerformance 사용
-  await measureRenders(<DefaultItemList />, {scenario, runs: 20});
+  await measureRenders(<DefaultItemList />, {scenario});
 });
